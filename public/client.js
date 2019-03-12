7563092e18b7b9f5b18c6d264e7e3a480d381b09
ExtraHash <ExtraHash@protonmail.com>
2019-03-12 10:00:52 -0600

    starting basic frontend

--- public/client.js
+++ public/client.js
@@ -1,13 +1,15 @@
 console.log('Client-side code running');
 
-var button = document.getElementById('button1');
+var button0 = document.getElementById('button0');
+var button1 = document.getElementById('button1');
 var button2 = document.getElementById('button2');
 var button3 = document.getElementById('button3');
 var button4 = document.getElementById('button4');
 var button5 = document.getElementById('button5');
 var button6 = document.getElementById('button6');
 var button7 = document.getElementById('button7');
-
+var button8 = document.getElementById('button8');
+var button9 = document.getElementById('button9');
 var buttonA = document.getElementById('buttonA');
 var buttonB = document.getElementById('buttonB');
 var buttonC = document.getElementById('buttonC');
@@ -16,91 +18,106 @@ var buttonE = document.getElementById('buttonE');
 var buttonF = document.getElementById('buttonF');
 var buttonG = document.getElementById('buttonG');
 
-
-
-
 var Bets = [];
 
-button.addEventListener('click', function(e) {
-  console.log('1 button was clicked');
-  Bets.push('1');
+button0.addEventListener('click', function(e) {
+  console.log('0 button was clicked');
+  Bets.push('0');
   console.log(Bets);
+});
+
+button1.addEventListener('click', function(e) {
+    console.log('1 button was clicked');
+    Bets.push('1');
+    console.log(Bets);
 });
 
 button2.addEventListener('click', function(e) {
-  console.log('2 button was clicked');
-  Bets.push('2');
-  console.log(Bets);
+    console.log('2 button was clicked');
+    Bets.push('2');
+    console.log(Bets);
 });
 
 button3.addEventListener('click', function(e) {
-  console.log('3 button was clicked');
-  Bets.push('3');
-  console.log(Bets);
+    console.log('3 button was clicked');
+    Bets.push('3');
+    console.log(Bets);
 });
 
 button4.addEventListener('click', function(e) {
-  console.log('4 button was clicked');
-  Bets.push('4');
-  console.log(Bets);
+    console.log('4 button was clicked');
+    Bets.push('4');
+    console.log(Bets);
 });
 
 button5.addEventListener('click', function(e) {
-  console.log('5 button was clicked');
-  Bets.push('5');
-  console.log(Bets);
+    console.log('5 button was clicked');
+    Bets.push('5');
+    console.log(Bets);
 });
 
 button6.addEventListener('click', function(e) {
-  console.log('6 button was clicked');
-  Bets.push('6');
-  console.log(Bets);
+    console.log('6 button was clicked');
+    Bets.push('6');
+    console.log(Bets);
 });
 
 button7.addEventListener('click', function(e) {
-  console.log('7 button was clicked');
-  Bets.push('7');
-  console.log(Bets);
+    console.log('7 button was clicked');
+    Bets.push('7');
+    console.log(Bets);
+});
+
+button8.addEventListener('click', function(e) {
+    console.log('8 button was clicked');
+    Bets.push('8');
+    console.log(Bets);
+});
+
+button9.addEventListener('click', function(e) {
+    console.log('9 button was clicked');
+    Bets.push('9');
+    console.log(Bets);
 });
 
 buttonA.addEventListener('click', function(e) {
-  console.log('a button was clicked');
-  Bets.push('a');
-  console.log(Bets);
+    console.log('a button was clicked');
+    Bets.push('a');
+    console.log(Bets);
 });
 
 buttonB.addEventListener('click', function(e) {
-  console.log('b button was clicked');
-  Bets.push('b');
-  console.log(Bets);
+    console.log('b button was clicked');
+    Bets.push('b');
+    console.log(Bets);
 });
 
 buttonC.addEventListener('click', function(e) {
-  console.log('c button was clicked');
-  Bets.push('c');
-  console.log(Bets);
+    console.log('c button was clicked');
+    Bets.push('c');
+    console.log(Bets);
 });
 
 buttonD.addEventListener('click', function(e) {
-  console.log('d button was clicked');
-  Bets.push('d');
-  console.log(Bets);
+    console.log('d button was clicked');
+    Bets.push('d');
+    console.log(Bets);
 });
 
 buttonE.addEventListener('click', function(e) {
-  console.log('e button was clicked');
-  Bets.push('e');
-  console.log(Bets);
+    console.log('e button was clicked');
+    Bets.push('e');
+    console.log(Bets);
 });
 
 buttonF.addEventListener('click', function(e) {
-  console.log('f button was clicked');
-  Bets.push('f');
-  console.log(Bets);
+    console.log('f button was clicked');
+    Bets.push('f');
+    console.log(Bets);
 });
 
 buttonG.addEventListener('click', function(e) {
-  console.log('g button was clicked');
-  Bets.push('g');
-  console.log(Bets);
+    console.log('g button was clicked');
+    Bets.push('g');
+    console.log(Bets);
 });
